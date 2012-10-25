feeb734f083ea9e208b0b595e7ec5d5219b67b34
Paolo Zaccagnini <paozac@gmail.com>
2012-10-25 10:19:27 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EtModelCom::Application.config.secret_token = 'ead0a240dfa4c779d00f23bfc9df1ec213f139a12a713b5b6f2dfbab0047958186a704c3268168fe46fa4819e88be5f439e185a60c6a2c2b3b9b7c538e100fcb'
