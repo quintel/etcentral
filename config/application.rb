3611e7607181ef5b5f82f1a1c91e905fe488d7bc
Paolo Zaccagnini <paozac@gmail.com>
2012-10-25 10:27:55 +0200

    configured generators

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,13 @@ module EtModelCom
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.template_engine :haml
+      g.test_framework :rspec
+      g.assets false
+      g.helpers false
+      g.view_specs false
+    end
   end
 end
