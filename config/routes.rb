bce106c35a3793c95caf4a95e807c7a1766cd49a
Nikolay Petrachkov <nikolay.petrachkov@quintel.com>
2012-11-19 17:09:10 +0100

    Move Disclaimer page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ EtModelCom::Application.routes.draw do
   root to: 'pages#index'
 
     match '/about'                        => 'pages#about'
+    match '/disclaimer'                   => 'pages#disclaimer'
 
 end
