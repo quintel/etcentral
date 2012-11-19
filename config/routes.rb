68ed17f64f8bef6fb99469aacfed2e030fff38ae
Nikolay Petrachkov <nikolay.petrachkov@quintel.com>
2012-11-19 17:11:39 +0100

    Move Privacy Statement page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ EtModelCom::Application.routes.draw do
 
     match '/about'                        => 'pages#about'
     match '/disclaimer'                   => 'pages#disclaimer'
+    match '/privacy_statement'            => 'pages#privacy_statement'
+
 
 end
