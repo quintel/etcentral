d6a8fba0296a5c72b5079bf30611e79f9c7a4522
Nikolay Petrachkov <nikolay.petrachkov@quintel.com>
2012-11-21 11:22:32 +0100

    Add root_link to logo

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 EtModelCom::Application.routes.draw do
   root to: 'pages#index'
 
-    match '/about'                         => 'pages#about'
-    match '/disclaimer'                   => 'pages#disclaimer'
-    match '/privacy_statement'        => 'pages#privacy_statement'
-    match '/press_releases'             => 'pages#press_releases'
-    match "/404", :to => "pages#404"
+  match '/about'                         => 'pages#about'
+  match '/disclaimer'                   => 'pages#disclaimer'
+  match '/privacy_statement'        => 'pages#privacy_statement'
+  match '/press_releases'             => 'pages#press_releases'
+  match "/404", :to => "pages#404"
 
 end
