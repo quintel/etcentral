6878e66eca0148017d5cbd3ebaaefd5ea4dac05a
Nikolay Petrachkov <nikolay.petrachkov@quintel.com>
2012-11-27 16:36:05 +0100

    Add translations for partners

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,6 +10,7 @@ module ApplicationHelper
     links.push text: t("header.press_releases") ,     url: 'press_releases'
     links.push text: t("header.privacy_statement") ,  url: 'privacy_statement'
     links.push text: t("header.disclaimer") ,         url: 'disclaimer'
+    links.push text: t("header.partners") ,           url: 'partners'
     unless APP_CONFIG[:standalone]
       links.push text: t("header.publications") ,     url: "http://refman.et-model.com", target: "_blank"
     end
