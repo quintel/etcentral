7551bc0769cd0272cf6dc9d4367485d5fb1e52bc
Nikolay Petrachkov <nikolay.petrachkov@quintel.com>
2013-01-02 11:23:28 +0100

    Clean deploy.rb. Close #24

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,6 @@
 set :application, "et-model.com"
 set :server_type, 'production'
 set :deploy_to, "/u/apps/et-model.com"
-set :db_host, "etm.cr6sxqj0itls.eu-west-1.rds.amazonaws.com"
 set :scm, :git
 set :repository,  "git@github.com:quintel/et-model.com.git"
 set :user, 'ubuntu'
@@ -11,15 +10,12 @@ set :chmod755, "app config db lib public vendor script script/* public/disp*"
 ssh_options[:forward_agent] = true
 set :use_sudo, false
 set :bundle_flags, '--deployment --quiet --binstubs --shebang ruby-local-exec'
-set :local_db_name, 'et-model.com_dev'
 
 set :branch, "production"
 set :domain, "et-model.com"
 set :rails_env, "production"
 set :application_key, "etmodel"
-# set :db_pass, "HaLjXwRWmu60DK"
-# set :db_name, application_key
-# set :db_user, application_key
-set :airbrake_key, "aadd4cc40d52dabf842d4dce932e84a3"
-server domain, :web, :app, :db, :primary => true
+
+set :airbrake_key, "<airbrake_key>"
+server domain, :web, :app, :primary => true
 
