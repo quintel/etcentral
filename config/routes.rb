88dc61f4e23f969763f57c33621a44fb0c02fba2
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2013-06-21 17:35:06 +0200

    Adding partners to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ ETM::Application.routes.draw do
 
   resources :pages, only: [:index, :show]
 
+  resources :partners, only: [:index, :show]
+
   match '/404'      => 'pages#not_found'
 
   root :to => 'pages#root'
