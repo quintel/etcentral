72dac1e5752bc19c074881a38b713642f39efd1a
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2013-06-21 14:18:53 +0200

    Argh, ugly formatting by Dennis - 1.year

--- app/models/yml_read_only_record.rb
+++ app/models/yml_read_only_record.rb
@@ -2,7 +2,7 @@ class YmlReadOnlyRecord
 
   def initialize(attr_hash)
     attr_hash.symbolize_keys!
-    attr_hash.each do |key,value|
+    attr_hash.each do |key, value|
       self.send("#{key}=", value)
     end
   end
@@ -16,7 +16,7 @@ class YmlReadOnlyRecord
   end
 
   def self.find(key)
-    self.all.select{|p|p.key == key}.first
+    self.all.select { |p| p.key == key }.first
   end
 
   #######
@@ -24,15 +24,15 @@ class YmlReadOnlyRecord
   #######
 
   def self.load_collective_file
-    file = "#{Rails.root}/config/#{yml_store}.yml"
-    return false unless File.exist? file
-    YAML.load_file(file).map {|i| self.new i }
+    file = "#{Rails.root}/config/#{ yml_store }.yml"
+    return false unless File.exist?(file)
+    YAML.load_file(file).map { |i| self.new i }
   end
 
   def self.load_directory
     items = []
-    Dir.glob("#{Rails.root}/config/#{yml_store}/*.yml") do |yml_file|
-      items << self.new( YAML.load_file yml_file )
+    Dir.glob("#{Rails.root}/config/#{ yml_store }/*.yml") do |yml_file|
+      items << self.new(YAML.load_file yml_file)
     end
     items
   end
