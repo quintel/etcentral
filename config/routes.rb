8958afc57422e550124e5ee98767dee8c39c21b0
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2013-07-17 15:58:02 +0200

    Improving on link to partner info

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ ETM::Application.routes.draw do
   resources :presets,         only: [:index, :show]
   resources :press_releases,  only: [:index, :show]
 
-  match '/404'      => 'pages#not_found'
+  match '/404' => 'pages#not_found'
+
+  match '/partners/info/:id' => 'pages#show', as: :partner_info
 
   root :to => 'pages#root'
 
