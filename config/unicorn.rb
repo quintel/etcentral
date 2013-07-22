23486beea91d550fca129c37b78644dc8da2d4c8
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2013-07-22 14:10:31 +0200

    cleaning up old leftovers

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 worker_processes 2
-working_directory '/u/apps/quintel/current'
+working_directory '/u/apps/etcentral/current'
 
 # This loads the application in the master process before forking worker
 # processes. Read more about it here:
@@ -12,14 +12,14 @@ timeout 30
 
 # This is where we specify the socket. We will point the upstream Nginx module
 # to this socket later on
-listen '/u/apps/quintel/shared/pids/unicorn.sock', backlog: 64
+listen '/u/apps/etcentral/shared/pids/unicorn.sock', backlog: 64
 
 # File containing the Unicorn process ID.
-pid '/u/apps/quintel/shared/pids/unicorn.pid'
+pid '/u/apps/etcentral/shared/pids/unicorn.pid'
 
 # Set the path of the log files inside the log folder of the testapp
-stderr_path '/u/apps/quintel/shared/log/unicorn.log'
-stdout_path '/u/apps/quintel/shared/log/unicorn.log'
+stderr_path '/u/apps/etcentral/shared/log/unicorn.log'
+stdout_path '/u/apps/etcentral/shared/log/unicorn.log'
 
 before_fork do |server, worker|
   # This option works in together with preload_app true setting. What is does
@@ -30,7 +30,7 @@ before_fork do |server, worker|
   # around until it is explicitly killed (so that it can be used if the new
   # master fails to start). Since we got as far as starting a new worker, we
   # end the old process...
-  old_pid = '/u/apps/quintel/shared/pids/unicorn.pid.oldbin'
+  old_pid = '/u/apps/etcentral/shared/pids/unicorn.pid.oldbin'
 
   if File.exists?(old_pid) && server.pid != old_pid
     begin
@@ -52,5 +52,5 @@ end
 # Without this Unicorn ignores USR2. See
 # http://qxjit.com/blog/headachenewunicorn-capistrano-bundler-usr2
 before_exec do |server|
-  ENV["BUNDLE_GEMFILE"] = "/u/apps/quintel/current/Gemfile"
+  ENV["BUNDLE_GEMFILE"] = "/u/apps/etcentral/current/Gemfile"
 end
