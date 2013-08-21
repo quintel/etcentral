5e529119391058f90a3d88a8264eb6914344fdd4
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2013-08-21 17:10:50 +0200

    fixing links again

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,13 +2,23 @@ module PagesHelper
 
   def product_link(product)
     link = "http://"
-    link += is_beta? ? 'beta.' : ''
+    link += has_beta?(product) ? 'beta.' : ''
     link += product.to_s
     link += dutch? ? '.energietransitiemodel.nl' : '.energytransitionmodel.com'
   end
 
   def is_beta?
     request.host_with_port =~ /beta/
+  end
+
+  def has_beta?(product)
+    return false unless is_beta?
+
+    case product
+    when :light then true
+    when :pro   then true
+    else false
+    end
   end
 
   def dutch?
