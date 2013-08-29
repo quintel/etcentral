0e872d8d94fae27039ab881222180f710981c1f1
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2013-08-29 09:22:42 +0200

    clean up deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,6 @@ set :user, "ubuntu"
 
 set :bundle_flags, '--deployment --quiet --binstubs --shebang ruby-local-exec'
 
-# Some files that will need proper permissions set
 set :chmod755, "app config db lib public vendor script script/* public/disp*"
 ssh_options[:forward_agent] = true
 set :use_sudo, false
@@ -30,17 +29,4 @@ task :production do
   server domain, :web, :app
 end
 
-
 after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
