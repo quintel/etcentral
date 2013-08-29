2aa1fd7a02942826bb6558bd5b926ac4557da1ba
Frans van Camp <fvancamp@xs4all.nl>
2013-08-29 16:29:38 +0200

    Delete config.yml altogether

--- config/initializers/load_config.rb
+++
@@ -1,6 +0,0 @@
-raw_config = File.read("#{Rails.root}/config/config.yml")
-APP_CONFIG = YAML.load(raw_config)[Rails.env].symbolize_keys
-
-Airbrake.configure do |config|
-  config.api_key = APP_CONFIG[:airbrake_api_key]
-end if APP_CONFIG[:airbrake_api_key].present?
