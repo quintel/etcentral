3c3a994fa9a3c0bb4297aac0fe8670a9ff6b98aa
ThomasMAC <tuds_wilkinson@hotmail.com>
2015-08-26 11:25:53 +0200

    Add redirects to RLI scenarios to config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 ETM::Application.routes.draw do
 
+  # Temporary: Redirect RLI scenarios to beta scenarios
+  match '/ser-scenario-2023'                    => redirect { 'http://beta.pro.et-model.com/scenarios/193349' }
+  match '/80-procent-co2-reductiescenario-2050' => redirect { 'http://beta.pro.et-model.com/scenarios/423879' }
+  match '/95-procent-co2-reductiescenario-2050' => redirect { 'http://beta.pro.et-model.com/scenarios/423882' }
+
   # match '/help' => 'articles#index', as: :help
 
   resources :pages,           only: [:index, :show]
