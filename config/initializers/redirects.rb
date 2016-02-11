85ec4ff97866f96f9c37929809766febc3d4d139
Gerard Westerhof <gerard@grdw.nl>
2016-02-11 10:00:44 +0100

    Setting custom etmodel redirects in frozen hash
    Fixes: #132

---
+++ config/initializers/redirects.rb
@@ -0,0 +1,7 @@
+REDIRECTS = {
+  '/rli/ser-scenario-2023'                    => 193349,
+  '/rli/80-procent-co2-reductiescenario-2050' => 423879,
+  '/rli/95-procent-co2-reductiescenario-2050' => 423882,
+  '/iabr/routekaartscenario-2035'             => 277819,
+  '/iabr/routekaartscenario-2025'             => 279280
+}.freeze
