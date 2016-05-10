e927c67c2ae2987f47682866066bd6b4ac73902b
Gerard Westerhof <gerard@grdw.nl>
2016-05-10 13:16:08 +0200

    Updating the IABR redirect to production

--- config/initializers/redirects.rb
+++ config/initializers/redirects.rb
@@ -2,6 +2,6 @@ REDIRECTS = {
   '/rli/ser-scenario-2023'                    => 'http://pro.et-model.com/scenarios/282929',
   '/rli/80-procent-co2-reductiescenario-2050' => 'http://pro.et-model.com/scenarios/423879',
   '/rli/95-procent-co2-reductiescenario-2050' => 'http://pro.et-model.com/scenarios/423882',
-  '/iabr/routekaartscenario-2035'             => 'http://beta.pro.et-model.com/scenarios/519784',
+  '/iabr/routekaartscenario-2035'             => 'http://pro.et-model.com/scenarios/291793',
   '/iabr/routekaartscenario-2025'             => 'http://pro.et-model.com/scenarios/279280'
 }.freeze
