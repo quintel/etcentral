bf6b6c1fb51c828578639941ea20e8ffdb520dd3
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2016-06-20 13:21:58 +0200

    Redirect for industrie-in-transitie

--- config/initializers/redirects.rb
+++ config/initializers/redirects.rb
@@ -3,5 +3,6 @@ REDIRECTS = {
   '/rli/80-procent-co2-reductiescenario-2050' => 'http://pro.et-model.com/scenarios/423879',
   '/rli/95-procent-co2-reductiescenario-2050' => 'http://pro.et-model.com/scenarios/423882',
   '/iabr/routekaartscenario-2035'             => 'http://pro.et-model.com/scenarios/291793',
-  '/iabr/routekaartscenario-2025'             => 'http://pro.et-model.com/scenarios/279280'
+  '/iabr/routekaartscenario-2025'             => 'http://pro.et-model.com/scenarios/279280',
+  '/industrie-in-transitie'                   => 'http://refman.energytransitionmodel.com/publications/2037'
 }.freeze
