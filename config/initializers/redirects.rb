5db49a21433bc138f43d637f9f6b698ea984e076
Dorine van der Vlies <dorine.vandervlies@quintel.com>
2017-05-24 14:13:12 +0200

    Update redirects.rb

--- config/initializers/redirects.rb
+++ config/initializers/redirects.rb
@@ -7,7 +7,7 @@ REDIRECTS = {
   '/industrie-in-transitie'                               => 'http://refman.energytransitionmodel.com/publications/2037',
   '/ameland'                                              => 'https://pro.energytransitionmodel.com/scenarios/301900',
   '/industrie-in-transitie/100-procent-duurzaam-2030'     => 'https://pro.energytransitionmodel.com/scenarios/311918',
-  '/industrie-in-transitie/80-procent-co2-reductie-2050'  => 'https://pro.energytransitionmodel.com/scenarios/309955'
-  '/urgenda'                                              => 'https://beta-pro.energytransitionmodel.com/scenarios/677057'
+  '/industrie-in-transitie/80-procent-co2-reductie-2050'  => 'https://pro.energytransitionmodel.com/scenarios/309955',
+  '/urgenda'                                              => 'https://beta-pro.energytransitionmodel.com/scenarios/677057',
   '/urgenda/bau'                                          => 'https://beta-pro.energytransitionmodel.com/scenarios/677051'
 }.freeze
