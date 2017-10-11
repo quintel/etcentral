fff8cadf51a8856c94234d27fd47ad4bff21f21a
Joris Berkhout <joris.berkhout@quintel.com>
2017-10-11 16:14:11 +0200

    Added redirects for Flevoland scenarios

--- config/initializers/redirects.rb
+++ config/initializers/redirects.rb
@@ -9,5 +9,8 @@ REDIRECTS = {
   '/industrie-in-transitie/100-procent-duurzaam-2030'     => 'https://pro.energytransitionmodel.com/scenarios/311918',
   '/industrie-in-transitie/80-procent-co2-reductie-2050'  => 'https://pro.energytransitionmodel.com/scenarios/309955',
   '/urgenda'                                              => 'https://pro.energytransitionmodel.com/scenarios/155680',
-  '/urgenda/bau'                                          => 'https://pro.energytransitionmodel.com/scenarios/325571'
+  '/urgenda/bau'                                          => 'https://pro.energytransitionmodel.com/scenarios/325571',
+  'flevoland/nulscenario-2050'                            => 'https://beta-pro.energytransitionmodel.com/scenarios/723887',
+  'flevoland/toekomstbeeld-2050'                          => 'https://beta-pro.energytransitionmodel.com/scenarios/734590',
+  'flevoland/toekomstbeeld-2035'                          => 'https://beta-pro.energytransitionmodel.com/scenarios/734596'
 }.freeze
