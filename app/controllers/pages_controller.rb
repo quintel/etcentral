51314854bad108ae2e63377adae6cb3e2f18fd94
Anthony Williams <hi@antw.dev>
2020-02-13 18:25:46 +0000

    PagesController#show 404s with HTML
    Fixes that the action catches Not Found routes with other formats (such
    as "/no.png") but doesn't have an appropriate templte.
    
    Resolves ETCENTRAL-1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,7 +9,7 @@ class PagesController < ApplicationController
       other_locale = I18n.locale.to_s == 'en' ? 'nl' : 'en'
       unless @page = Page.find(params[:id], other_locale)
         # raise ActionController::RoutingError.new("Page '#{ params[:id] }' not found")
-        render 'pages/not_found'
+        render 'pages/not_found.html'
       else
         flash[:notice] = I18n.t('pages.not_available_in_this_locale')
       end
