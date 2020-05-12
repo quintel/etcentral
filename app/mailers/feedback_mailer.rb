74e9c093b60c72cff4c7bd643659558064e35fed
noracato <ncschinkel@gmail.com>
2020-05-12 14:48:02 +0200

    Add feedback form to the contact page

---
+++ app/mailers/feedback_mailer.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Notifies quintel of feedback being sent through the feedback form
+class FeedbackMailer < ActionMailer::Base
+  def feedback_email(options)
+    @options = options
+
+    mail to: 'info@energytransitionmodel.com',
+         from: options[:email],
+         subject: 'ETM Feedback',
+         body: options[:message]
+  end
+end
