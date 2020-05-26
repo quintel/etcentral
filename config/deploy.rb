a7536a7a473fe5c8a8c6f8173dbc639b7ee74de2
Anthony Williams <hi@antw.dev>
2020-05-26 10:40:41 +0100

    Link email.yml when deploying

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,7 @@ set :bundle_binstubs, (-> { shared_path.join('sbin') })
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w[config/secrets.yml]
+set :linked_files, %w[config/secrets.yml config/email.yml]
 
 # Default value for linked_dirs is []
 set :linked_dirs,
