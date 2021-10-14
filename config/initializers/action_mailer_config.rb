71d8b61f9f57268525afd2876f67fd43dbb9def8
Anthony Williams <hi@antw.dev>
2021-10-14 16:13:52 +0100

    Remove email.yml; use environment when set

--- config/initializers/action_mailer_config.rb
+++ config/initializers/action_mailer_config.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
-if Rails.env.production? || Rails.env.staging?
-  email_settings = YAML.safe_load(
-    File.read("#{Rails.root}/config/email.yml"),
-    symbolize_names: true
-  )
-
-  if email_settings[Rails.env.to_sym]
-    ActionMailer::Base.smtp_settings = email_settings[Rails.env.to_sym]
-  end
+if (Rails.env.staging? || Rails.env.production?) && ENV['MAILER_SMTP_SETTINGS'].present?
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = JSON.load(ENV['MAILER_SMTP_SETTINGS'])
+  ActionMailer::Base.default_url_options = { host: ENV['MAILER_HOST'] }
+  ActionMailer::Base.asset_host = ENV['MAILER_HOST']
 end
