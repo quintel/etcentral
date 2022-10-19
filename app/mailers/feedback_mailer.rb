9496bf718fd6452c6438f8ab130ee04f42836cce
Anthony Williams <hi@antw.dev>
2022-10-19 16:19:17 +0100

    Add another common spam word to the feedback filter

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -10,9 +10,15 @@ class FeedbackMailer < ActionMailer::Base
   # Public: Returns if the message is one we want to send. Silently rejects spam messages about
   # domain renewals.
   def self.allowed_message?(message)
-    message = message.to_s.downcase
-    !(message.include?('domain') && message.include?('renew'))
+    !prohibited_content?(message.to_s.downcase)
   end
+
+  def self.prohibited_content?(message)
+    (message.include?('renew') && message.include?('domain')) ||
+      message.include?('porn')
+  end
+
+  private_class_method :prohibited_content?
 
   # Public: Returns if the sender's e-mail is allowed to send messages. Silently rejects messages
   # from spammers (which, for some reason, seem to use our own address in the form).
